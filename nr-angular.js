8d20444d8a3826c51066ceb91fc34160b5c9c3e0
Praneet Sharma <psharma@newrelic.com>
2015-12-20 11:52:46 -0800

    navstart null

--- nr-angular.js
+++ nr-angular.js
@@ -50,6 +50,7 @@
       var navEnd = this.measure('navEnd', 'navStart');
       var renderTime = this.measure('pageRendered', 'navStart');
       this.NREUM.addPageAction('Route', {'url': fragmentName, 'appTime': navEnd, 'renderTime': renderTime - this.debounceTime});
+      this.marks['navStart'] = null;
     };
 
     this.checkBeaconRequirements = function() {
